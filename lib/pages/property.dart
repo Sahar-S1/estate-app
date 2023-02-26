f7e4b79bcb27a80d03b4eb9d1ccf8e5dd4c12c27
Sahil Makhijani <sahil.makhijani@gmail.com>
2023-02-26 22:31:45 +0530

    Update property page content design

--- lib/pages/property.dart
+++ lib/pages/property.dart
@@ -18,15 +18,18 @@ class PropertyPage extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: Text("Estate - ${property.address_name}"),
+        elevation: 0,
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.transparent,
       ),
       body: Stack(
         children: [
           Stack(
             children: [
               Container(
-                height: size.height * 0.3,
+                height: size.height * 0.4,
                 decoration: BoxDecoration(
                   image: DecorationImage(
                     image: NetworkImage(
@@ -43,6 +46,7 @@ class PropertyPage extends StatelessWidget {
                 ),
               ),
               Positioned(
+                top: size.height * .075,
                 left: size.width * .1,
                 child: Hero(
                   tag: property.id,
@@ -60,7 +64,7 @@ class PropertyPage extends StatelessWidget {
             alignment: Alignment.bottomCenter,
             child: Container(
               height: size.height * 0.6,
-              decoration: BoxDecoration(
+              decoration: const BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.vertical(
                   top: Radius.circular(20.0),
@@ -68,12 +72,118 @@ class PropertyPage extends StatelessWidget {
               ),
               child: ListView(
                 scrollDirection: Axis.vertical,
-                shrinkWrap: true,
+                padding: const EdgeInsets.all(16),
                 children: [
-                  Container(
-                    padding: const EdgeInsets.all(8.0),
-                    child: getPropertyTable(theme, property),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              property.address_name,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Icon(
+                                  Icons.place_outlined,
+                                  size: 13,
+                                ),
+                                SizedBox(width: 3),
+                                Text(
+                                  property.address_city,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Text(
+                        '₹ ${(property.market_value / 10000000).toStringAsFixed(2)} Cr',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: theme.colorScheme.primary,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
                   ),
+                  Container(
+                    height: 100,
+                    margin: EdgeInsets.symmetric(vertical: 16),
+                    child: StatefulBuilder(
+                      builder: (context, setState) {
+                        var items = [
+                          {
+                            "icon": Icons.bed,
+                            "value": "${property.rooms} Bedrooms",
+                          },
+                          {
+                            "icon": Icons.straighten,
+                            "value": "${property.carpet_area} m^2"
+                          },
+                          if (property.facilities.contains("water_supply"))
+                            {"icon": Icons.water_drop, "value": "Water Supply"},
+                          if (property.facilities.contains("security"))
+                            {"icon": Icons.security, "value": "Security"},
+                          if (property.facilities.contains("backup_power"))
+                            {
+                              "icon": Icons.battery_saver,
+                              "value": "Backup Power"
+                            },
+                          if (property.facilities.contains("parking"))
+                            {"icon": Icons.local_parking, "value": "Parking"},
+                          if (property.amenities.contains("swimming_pool"))
+                            {"icon": Icons.pool, "value": "Swimming Pool"},
+                          if (property.amenities.contains("gym"))
+                            {"icon": Icons.fitness_center, "value": "Gym"},
+                          if (property.amenities.contains("park"))
+                            {"icon": Icons.park, "value": "Park"},
+                          if (property.amenities.contains("club_house"))
+                            {"icon": Icons.sports_tennis, "value": "Clubhouse"},
+                        ];
+                        return ListView.separated(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: items.length,
+                          itemBuilder: (context, index) => Container(
+                            width: 100,
+                            height: 100,
+                            padding: const EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: theme.primaryColor,
+                            ),
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: Icon(items[index]["icon"] as IconData),
+                                ),
+                                Text(items[index]["value"] as String),
+                              ],
+                            ),
+                          ),
+                          separatorBuilder: (context, index) =>
+                              const SizedBox(width: 10),
+                        );
+                      },
+                    ),
+                  ),
+                  const Divider(thickness: 2),
+                  getPropertyTable(theme, property),
                 ],
               ),
             ),
